567f52bc6a77c5dfd350590e57084b9a64309ee2
Med Mahmoud <med@temple.edu>
2013-01-22 16:18:31 -0500

    Changed the sign of the cost function for clarity.
    We need the opposite sign to *minimize* the cost, but we can add that in
    later.

--- fitter.py
+++ fitter.py
@@ -13,15 +13,15 @@ def positive_definite_cost(mat):
   """
   Returns a measure of how far ``mat`` is from being positive definite.
 
-  The measure is the magnitude of the sum of the negative minor determinants.
+  The measure is the sum of the negative minor determinants.
 
   If the matrix IS positive definite, it measures *how* positive definite
   it is by computing the sum of its *positive* minor determinants (all of
-  them will be positive).  The answer in this case will be negative.
+  them will be positive).  
   """
   minors = minor_determinants(mat)
-  indefinite = [-a for a in minors if a<0]
-  definite   = [-a for a in minors if a>0]
+  indefinite = [a for a in minors if a<0]
+  definite   = [a for a in minors if a>0]
 
   if len(indefinite)!=0: #the matrix is not positive definite
     return sum(indefinite)
