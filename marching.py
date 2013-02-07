0ce06b7de4f75133f46d390f176abbd2e2693619
Med Mahmoud <med@temple.edu>
2013-02-07 12:58:36 -0500

    Can now compute area of the iso-intersection in 2d

--- marching.py
+++ marching.py
@@ -31,13 +31,62 @@ def iso_intersect_dists(ptvals,val=0):
   simplex whose vertices have values ``ptvals``.
   """
   #find vertices above and below isovalue
-  lows = np.where(ptvals<val)
+  lows = np.where(ptvals<=val) #the interpolation will perturb equal values
   his  = np.where(ptvals>val)
-  #TODO (should check for isovalues at vertices)
   #for each vertex below the isovalue, compute the fraction of the distance
   # to vertices *above* the isovalue that the isovalue is at
   ivD = np.array([[linear_interpolate(l,h) for h in ptvals[his]] for l in ptvals[lows]])
   #return the low indices and relative distances from lows to isovalues
-  return (lows,his, ivD)
+  return (ivD, lows,his)
+
+def triangle_iso_area(pts,ivD,lows,his):
+  """
+  Given the vertices of a triangle, ``pts``, and the distances along the edges
+  to the isovalue, ``ivDs``, compute the area of the *admissible* region
+  inside the triangle.
+  """
+  #We want to find the area of the simplest region, either the admissible or
+  # inadmissible side.  If the inadmissible side is easier, compute as if it was
+  # the admissible side, but then later we need to flip the answer.
+  #I.E., we invert if there are fewer LOW vertices than HIGH vertices, because
+  # we want to work with as few "different" vertices as possible.
+  if ivD.shape[0]<ivD.shape[1]:
+    invert=True
+    ivD = ivD.T
+  else:
+    invert=False
+  
+  #For a triangle, there are two cases for the intersection:
+  # I  - all the vertices have the same sign
+  #   Return the entire area of the triangle
+  #
+  # II - one vertex is different
+  #   Return the area of the triangle bounded by the isosurface
+  if 3 in ivD.shape:
+    translated_verts = pts - pts[0]
+    area = triangle_area(*translated_verts[1:])
+  else:
+    #Determine the different vertex.  If we haven't inverted, this will be the
+    # sole HIGH vertex.  If we DID invert, this will be the sole LOW vertex.
+    if invert:
+      vtx0 = lows[0]
+    else:
+      vtx0 = his[0]
+    #Now translate the triangle so the different vertex is at the origin
+    pts0 = pts - vtx0
+    #Scale the lengths of the other edges to the isosurface distance
+    pts1 = pts0*ivD.T
+    #Compute the area of this triangle
+    area = triangle_area(*pts1[1:])
+
+  #Uninvert if necessary, and return the area
+  if invert:
+    whole_tri = triangle_area(*(pts-pts[0])[1:])
+    return whole_tri - area
+  else:
+    return area
+
+
+
 
 
