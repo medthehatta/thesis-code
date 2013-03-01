e659cf087130b15d80c51552de366051a14b66d0
Med Mahmoud <med@temple.edu>
2013-03-01 15:09:02 -0500

    Trivial spacing tweaks on tuesday_golriz

--- tuesday_golriz.py
+++ tuesday_golriz.py
@@ -6,21 +6,21 @@ import data_process
 import xlrd
 import matplotlib.pyplot as plt
 
-book      = xlrd.open_workbook("/home/med/re/data/golriz-biaxial/h4t1.xls")
+book = xlrd.open_workbook("/home/med/re/data/golriz-biaxial/h4t1.xls")
 
 nolo_pqrs = [1,-1]*np.reshape(book.sheet_by_name("NoLoPQRS").row_values(0),(4,2))
 
 pqrs_book = book.sheet_by_name('DynPQRS')
-pqrs      = np.reshape([pqrs_book.row_values(i) for i in range(pqrs_book.nrows)],(pqrs_book.nrows,4,2))
+pqrs = np.reshape([pqrs_book.row_values(i) for i in range(pqrs_book.nrows)],(pqrs_book.nrows,4,2))
 
 (Lx,Ly,Lz) = book.sheet_by_name("Dimension").row_values(0)
 
 forces = np.transpose([book.sheet_by_name("Force").col_values(0),book.sheet_by_name("Force").col_values(1)])
 
 def plot_deformation(verts):
-    ax =plt.gcf().add_subplot(111)
+    ax = plt.gcf().add_subplot(111)
     for v in verts:
-        poly=plt.Polygon(v,fill=False)
+        poly = plt.Polygon(v,fill=False)
         ax.add_patch(poly)
     plt.axis('scaled')
     plt.show()
