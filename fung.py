9880ec61f19714ff8f1d580f0190f01d9e1c2d74
Med Mahmoud <med@temple.edu>
2013-03-05 13:01:17 -0500

    Attempt to vectorize fung model.  May not work

--- fung.py
+++ fung.py
@@ -7,6 +7,49 @@ import lintools as lin
 import numpy as np
 import pdb
 
+def orthotropic_stiffness(Ex,Ey,Ez,nyz,nzx,nxy,Gyz,Gzx,Gxy):
+  """
+  Returns an orthotropic stiffness tensor with the given parameters.
+  """
+  shear_moduli = 2*np.array([Gyz,Gzx,Gxy])
+  
+  # The formula for the stiffness tensor that I have uses all kinds of weird
+  # versions of the poisson ratios, so we do the conversion first in order to
+  # use the formula verbatim
+  nzy = Ez/Ey*nyz
+  nxz = Ex/Ez*nzx
+  nyx = Ey/Ex*nxy
+
+  # Another intermediate variable
+  D = (1 - nxy*nyx - nyz*nzy - nzx*nxz - 2*nxy*nyz*nzx)/(Ex*Ey*Ez)
+
+  # Construct the upper triangle of the "normal" stress block in Voigt
+  # notation
+  N11 = (1 - nzy*nzy)/(Ey*Ez*D)
+  N12 = (nyx + nzx*nyz)/(Ey*Ez*D)
+  N13 = (nzx + nyz*nzy)/(Ey*Ez*D)
+  N22 = (1 - nzx*nxz)/(Ez*Ez*D)
+  N23 = (nzy + nzx*nxy)/(Ez*Ex*D)
+  N33 = (1 - nxy*nyz)/(Ex*Ey*D)
+
+  # Put in the triangle and then symmetrize
+  N0 = np.array([[N11,2*N12,2*N13],[0,N22,2*N23],[0,0,N33]])
+  N = (N0 + N0.T)/2.
+
+  # The whole Voigt stiffness (but expanded to be fully 9x9) is given by
+  shear_part = np.diagflat(shear_moduli)
+  C = lin.direct_sum(N,lin.direct_sum(shear_part,shear_part))
+
+  # Now we need to turn this into a bona-fide 4th rank tensor
+  return lin.reorder_matrix(C,lin.VOIGT_ORDER).reshape((3,3,3,3))
+
+
+def model(F,*params):
+  """
+  Exposed fung_P.
+  """
+  return fung_P(F,params[0],orthotropic_stiffness(*params[1:]))
+
 def fung(F,c,CC):
   """
   Returns the strain energy given the deformation, ``F``, and the model
@@ -25,18 +68,23 @@ def fung_P(F,c,CC):
   parameters.
   """
   dimension = F.shape[-1]
-  I = np.eye(dimension)
-  E = 0.5*np.dot(F.T,F) - 0.5*I
-  Q = np.einsum('abcd,ab,cd',CC,E,E)
+  if len(F.shape)>2:
+    I = np.tile(np.eye(dimension),(F.shape[0],1,1))
+    CC = np.tile(CC,(F.shape[0],1,1,1,1))
+  else:
+    I = np.eye(dimension)
+  E = 0.5*np.einsum('...ab,...ac',F,F) - 0.5*I
+  Q = np.einsum('...abcd,...ab,...cd',CC,E,E)
 
   # Derivative of Q wrt E: (rank 2)
-  dQdE = 2*np.einsum('abcd,cd',CC,E)
+  dQdE = 2*np.einsum('...abcd,...cd',CC,E)
   # Derivative of E wrt F: (rank 4)
-  dEdF = 0.5*np.einsum('ab,cd->adcb',I,F) + 0.5*np.einsum('ab,cd->bdca',I,F)
+  dEdF = 0.5*np.einsum('...ab,...cd->...adcb',I,F) + 0.5*np.einsum('...ab,...cd->...bdca',I,F)
   # Derivative of Q wrt F: (dQdE)ij (dEdF)ijkl (rank 2)
-  dQdF = np.einsum('ij,ijkl',dQdE,dEdF)
+  dQdF = np.einsum('...ij,...ijkl',dQdE,dEdF)
   # Return value: c/2 exp(Q) dQdF
-  return 0.5*c*np.exp(Q)*dQdF
+  # (numpy needs dQdF.T to properly multiply with np.exp(Q))
+  return 0.5*c*(np.exp(Q)*dQdF.T).T
 
 def fung_D(F,c,CC):
   """
