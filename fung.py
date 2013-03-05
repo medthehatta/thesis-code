fad4a004a82e8f6291f45e39e703a0b912462bc7
Med Mahmoud <med@temple.edu>
2013-03-05 14:10:48 -0500

    Finished vectorizing Fung functions

--- fung.py
+++ fung.py
@@ -50,6 +50,12 @@ def model(F,*params):
   """
   return fung_P(F,params[0],orthotropic_stiffness(*params[1:]))
 
+def model_D(F,*params):
+  """
+  Exposed fung_D with fully orthotropic stiffness.
+  """
+  return fung_D(F,params[0],orthotropic_stiffness(*params[1:]))
+
 
 def model_isotropic(F,*params):
   """
@@ -60,6 +66,15 @@ def model_isotropic(F,*params):
   G = E/(2*(1+n))
   return fung_P(F,params[0],orthotropic_stiffness(E,E,E,n,n,n,G,G,G))
 
+def model_isotropic_D(F,*params):
+  """
+  Exposed fung_D with isotropic stiffness.
+  Params are Young's modulus and Poisson's ratio.
+  """
+  (E,n) = params
+  G = E/(2*(1+n))
+  return fung_D(F,params[0],orthotropic_stiffness(E,E,E,n,n,n,G,G,G))
+
 
 def fung(F,c,CC):
   """
@@ -69,8 +84,13 @@ def fung(F,c,CC):
   Q = E:CC:E
   """
   dim = F.shape[-1]
-  E = 0.5*np.dot(F.T,F) - 0.5*np.eye(dim)
-  Q = np.einsum('abcd,ab,cd',CC,E,E)
+  if len(F.shape)>2:
+    I = np.tile(np.eye(dim),(F.shape[0],1,1))
+    CC = np.tile(CC,(F.shape[0],1,1,1,1))
+  else:
+    I = np.eye(dim)
+  E = 0.5*np.einsum('...ab,...ac',F,F) - 0.5*I
+  Q = np.einsum('...abcd,...ab,...cd',CC,E,E)
   return 0.5*c*(np.exp(Q) - 1)
 
 def fung_P(F,c,CC):
@@ -103,27 +123,31 @@ def fung_D(F,c,CC):
   parameters.
   """
   dimension = F.shape[-1]
-  I = np.eye(dimension)
-  E = 0.5*np.dot(F.T,F) - 0.5*I
-  Q = np.einsum('abcd,ab,cd',CC,E,E)
+  if len(F.shape)>2:
+    I = np.tile(np.eye(dimension),(F.shape[0],1,1))
+    CC = np.tile(CC,(F.shape[0],1,1,1,1))
+  else:
+    I = np.eye(dimension)
+  E = 0.5*np.einsum('...ab,...ac',F,F) - 0.5*I
+  Q = np.einsum('...abcd,...ab,...cd',CC,E,E)
 
   # Derivative of Q wrt E: (rank 2)
-  dQdE = 2*np.einsum('abcd,cd',CC,E)
+  dQdE = 2*np.einsum('...abcd,...cd',CC,E)
   # Derivative of E wrt F: (rank 4)
-  dEdF = 0.5*(np.einsum('ab,cd->adcb',I,F) + np.einsum('ab,cd->bdca',I,F))
+  dEdF = 0.5*(np.einsum('...ab,...cd->...adcb',I,F) + np.einsum('...ab,...cd->...bdca',I,F))
   # Derivative of Q wrt F: (dQdE)ij (dEdF)ijkl (rank 2)
-  dQdF = np.einsum('ij,ijkl',dQdE,dEdF)
+  dQdF = np.einsum('...ij,...ijkl',dQdE,dEdF)
 
   # Second derivative of Q wrt E: (rank 4)
   ddQdEdE = 2*CC
   # Second derivative of E wrt F: (rank 6)
-  ddEdFdF = 0.5*(np.einsum('ij,kl,mn->kmilnj',I,I,I) + 
-                 np.einsum('ij,kl,mn->kmjlni',I,I,I))
+  ddEdFdF = 0.5*(np.einsum('...ij,...kl,...mn->...kmilnj',I,I,I) + 
+                 np.einsum('...ij,...kl,...mn->...kmjlni',I,I,I))
 
   # Each term of 3-part product rule
   ddW1 = lin.tensor(dQdF,dQdF)
-  ddW2 = np.einsum('ijkl,ijmn,klpq',ddQdEdE,dEdF,dE,dF)
-  ddW3 = np.einsum('ij,ijklmn',dQdE,ddEdFdF)
+  ddW2 = np.einsum('...ijkl,...ijmn,...klpq',ddQdEdE,dEdF,dEdF)
+  ddW3 = np.einsum('...ij,...ijklmn',dQdE,ddEdFdF)
   # Return the sum of these, all times c/2 exp(Q)
-  return 0.5*c*np.exp(Q)*(ddW1 + ddW2 + ddW3)
+  return 0.5*c*(np.exp(Q)*(ddW1 + ddW2 + ddW3).T).T
 
