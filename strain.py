a9bdf06ab49a0f033f6cb1cabeb74bc96b34d33b
Med Mahmoud <med@temple.edu>
2013-03-07 18:04:29 -0500

    Vectorized biaxial deformation function

--- strain.py
+++ strain.py
@@ -8,8 +8,8 @@ import numpy as np
 
 def simple_shear(gamma,e1=np.array([1,0,0]),e2=np.array([0,1,0])):
   """
-  Returns simple shear of magnitude ``gamma`` in the plane spanned by ``e1`` and
-  ``e2``.
+  Returns simple shear of magnitude ``gamma`` in the plane spanned by ``e1``
+  and ``e2``.
   """
   return np.eye(3) + gamma*np.outer(e1,e2)
 
@@ -27,3 +27,27 @@ def biaxial_extension(stretch1,stretch2,
   return np.eye(3) + (stretch1-1)*np.outer(e1,e1) + \
                      (stretch2-1)*np.outer(e2,e2)
 
+def biaxial_extension_vec(stretches):
+  """
+  Biaxial extension function which supports our naiive calling from datafit.py
+  """
+  # We are going to add this deformation to the identity, so subtract off the
+  # "undeformed" part of the stretches
+  normalized = stretches - np.array([1.,1.])
+  # Reshape the stretches so they can be multiplied by projection matrices
+  normalized_r = normalized[:,:,None,None]
+
+  # Compute the projection matrices along the fibers
+  # The fibers are fixed for now as e1=i, e2=j
+  axes = np.eye(3)[:2]
+  # Projection operators are just the outer product of each axis with itself
+  projections = np.einsum('...i,...j',axes,axes)
+
+  # Compute the stretch matrices for each stretch
+  stretch_operators = normalized_r*projections
+
+  # Add these to the identity to get the total deformations
+  return np.eye(3) + stretch_operators
+  
+
+  
