fcf48edf146a075822557a2ee5d3b17ba01bddcb
Med Mahmoud <med@temple.edu>
2013-03-06 19:42:11 -0500

    Added glue between the arguments and the fitter.

--- interface.py
+++ interface.py
@@ -15,11 +15,11 @@ def argument_parse(argv=None):
   """
   Returns the argument namespace from the given values.
   """
-  parser_description = "Performs a constrained least-squares fit to"\
-                       "tissue data provided in a PK1 stress file and"\
-                       "a deformation gradient file aligned with it.  "\
-                       "Attempts to produce a fit which is Drucker stable"\
-                       "in the strain regime specified."
+  parser_description = 'Performs a constrained least-squares fit to'\
+                       'tissue data provided in a PK1 stress file and'\
+                       'a deformation gradient file aligned with it.  '\
+                       'Attempts to produce a fit which is Drucker stable'\
+                       'in the strain regime specified.'
   parser = argparse.ArgumentParser(description=parser_description)
 
   parser.add_argument('-P','--stress',
@@ -44,11 +44,38 @@ def argument_parse(argv=None):
   stresses = dat.numpy_array_from_file(parsed.stress,DELIMS)
   deformations = dat.numpy_array_from_file(parsed.deformation,DELIMS)
 
-  # Read in the models that will be tried.  Each "model" consists of the actual
+  # Read in the models that will be tried.  Each 'model' consists of the actual
   # model itself (file and function), initial parameter guesses, and
   # deformation bounds.
   models = yaml.load(open(parsed.model))
 
   return (stresses, deformations, models)
+
+def run_from_args(stresses, deformations, models):
+  """
+  Perform a fit for each given model with the given stress and deformation
+  data.  Presumably this is called with the return value from
+  ``argument_parse``.
+  """
+  DELIMS = ['\n',' ']
+  FITS = {}
+  # For each model specified, perform the fit and store the data
+  for m in models:
+    if not m.get('ignore'):
+      # Get the tissue model function
+      model_module = imp.load_source('model',m['module'])
+      model = getattr(model_module,m['function'])
+      # Set the desired stable deformation region
+      low = dat.numpy_array_from_string(m['strain-low'],DELIMS)
+      high = dat.numpy_array_from_string(m['strain-high'],DELIMS)
+      # Define the cost function for the optimization
+      # TODO: not constrained yet
+      cost = lambda p: fit.data_leastsqr(deformations,stresses,model,*p)
+      # Set the initial guess for the fit
+      initial = dat.numpy_array_from_string(m['initial'],DELIMS)
+      # Perform the fit and store the data
+      FITS[m['name']]=so.fmin(cost,initial,retall=True)
+
+  return FITS
 
 
