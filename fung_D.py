49feaf47ec440db4dec792d9c09e4be35ec98cb4
Med Mahmoud <med@temple.edu>
2013-04-08 09:15:23 -0400

    Separate symbolic and numeric.  Give everything shorter names.

--- fung_D.py
+++ fung_D.py
@@ -39,7 +39,7 @@ def initialize():
 
     # Attempt to load this from a pickle file
     try:
-        D_symbolic = pickle.load(open("fung_D_symbolic.pkl",'rb'))
+        Dsym = pickle.load(open("fung_Dsym.pkl",'rb'))
 
     # Otherwise, just recompute it
     except Exception:
@@ -52,52 +52,77 @@ def initialize():
             dQ[i] = sp.diff(Qee,f[i])
 
         # Calculate second derivatives
-        D_symbolic = np.empty((9,9),dtype=object)
-        symfile = open("fung_D_symbolic.pkl",'wb')
+        Dsym = np.empty((9,9),dtype=object)
+        symfile = open("fung_Dsym.pkl",'wb')
         for i in range(9):
             for j in range(i,9):
                 print("Symbolic ddQ_{0}{1}".format(i,j))
                 dQi = dQ[i]
                 dQj = dQ[j]
                 dQij = sp.diff(dQi,f[j])
-                D_symbolic[i,j] = dQi*dQj + dQij
+                Dsym[i,j] = dQi*dQj + dQij
 
                 # Optimize the derivative by substituting for J, and for
                 # products of f components
                 print("  Simplifying...")
                 print("  J  ",end="")
                 sys.stdout.flush()
-                D_symbolic[i,j].subs(J,sp.symbols('J'))
+                Dsym[i,j].subs(J,sp.symbols('J'))
                 for k in range(9):
                     for l in range(k,9):
                         print("f{}f{}".format(k,l),end="  ")
                         sys.stdout.flush()
                         pair_symbol = sp.symbols('ff_{0}{1}'.format(k,l))
-                        D_symbolic[i,j] = D_symbolic[i,j].subs(f[k]*f[l],pair_symbol)
+                        Dsym[i,j] = Dsym[i,j].subs(f[k]*f[l],pair_symbol)
                 # Since D will be symmetric, assign the symmetric components
                 print("\n  Symmetrizing...")
-                D_symbolic[j,i] = D_symbolic[i,j]
-                # This computation is pretty costly, so let's save it frequently
-                pickle.dump(D_symbolic, symfile)
+                Dsym[j,i] = Dsym[i,j]
+                # This computation is pretty costly, so let's save it
+                # frequently
+                pickle.dump(Dsym, symfile)
         symfile.close()
 
+    return Dsym
+
+def make_numeric(Dsym):
     # Transform each symbolic expression into a python function
     # We'll need the products of f components
+    # Construct the quadratic form
+    Q = np.empty((6,6),dtype=object)
+    for i in range(6):
+        for j in range(i,6):
+            Q[i,j]=sp.symbols('q_{i}{j}'.format(i=i,j=j))
+            Q[j,i]=Q[i,j]
+
+    # Flat list of independent entries of Q
+    q = lin.utri_flat(Q)
+
+    # Components of F
+    f = np.array([sp.symbols('f_{i}'.format(i=i)) for i in range(9)])
+
+    # Products of F components
     ff = lin.utri_flat(np.outer(f,f))
-    D_numeric = np.empty((9,9),dtype=object)
+
+    # Put everything into the numeric array
+    Dnum = np.empty((9,9),dtype=object)
     for i in range(9):
         for j in range(i,9):
             print("Numeric D_{0}{1}".format(i,j))
-            arguments = [ob.tolist() for ob in [q,f,ff]] + [sp.symbols('J')]
-            D_numeric[i,j] = sp.lambdify(arguments,D_symbolic[i,j])
-            D_numeric[j,i] = D_numeric[i,j]
-    return (D_symbolic,D_numeric)
+
+            # This will take the quadratic form, f, products of f, and J as
+            # arguments
+            arguments = [ob.tolist() for ob in [q,f,ff]] + \
+                        [sp.symbols('J')]
+            Dnum[i,j] = sp.lambdify(arguments,Dsym[i,j])
+            Dnum[j,i] = Dnum[i,j]
+
+    return Dnum
 
 # Pass a deformation F and a set of parameters (c,b1...b9)
-def D(F,c,bs,D_numeric):
+def D(F,c,bs,Dnum):
     f = F.ravel()
     C = fung.make_quad_form(*bs)
     CC = lin.utri_flat(el.voigt(C)[:6,:6])
     arglist = CC.tolist() + f.tolist()
-    return np.array([[D(*arglist) for D in DD] for DD in D_numeric])
+    return np.array([[D(*arglist) for D in DD] for DD in Dnum])
 
