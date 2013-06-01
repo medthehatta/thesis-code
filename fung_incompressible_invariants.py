3afb5fad0450d3cf36e96b6a4a25c1a17cbabf84
Med Mahmoud <med@temple.edu>
2013-06-01 12:48:15 -0400

    Clean up Qbar in fung_i_i

--- fung_incompressible_invariants.py
+++ fung_incompressible_invariants.py
@@ -1,46 +1,78 @@
+"""
+fung_incompressible_invariants.py
 
-def standard_orthotropic_P():
-    e1 = [1,0,0]
-    e2 = [0,1,0]
-    P = np.outer(e1,e1) - np.outer(e2,e2)
+Attempt to implement the invariant version of fung including incompressibility.
+Hopefully it isn't too slow and annoying to call.
+"""
+
+import elastic as el
+import lintools as lin
+from itertools import count
 
 
+def Qbar(E,c,M,L,P=None):
+    """
+    Computes the distortional ``Q`` in the invariant Fung model.
 
-def Qbar(E,c,m1,m2,m3,l11,l12,l13,l22,l23,l33,P=None):
-    P = P or standard_orthotropic_P()
+    Required arguments:
+     - Modified Lagrangian strain ``E``
+     - Scalar parameter ``c``
+     - List of 3 parameters ``M``
+     - List of 6 "upper triangle" parameters ``L``
+
+    Optional arguments:
+     - Orthotropic structure tensor ``P``
+
+    Returns:
+     - Distortional ``Q`` (a scalar)
+    """
     
-    I = np.eye(3)
+    # Get orthotropic projection operators
+    P = P or el.standard_orthotropic_P()
+    A = el.orthotropic_projectors(P)
 
-    M = [m1,m2,m3]
-    L = [l11,l12,l13,l22,l23,l33]
-                
-    PP = np.dot(P,P)
-    A1 = 0.5*(P + PP)
-    A2 = 0.5*(P - PP)
-    A3 = I - A1 - A2
-    A = [A1,A2,A3]
-       
+    # Compute scalar invariants
     EE = np.dot(E,E)
-    invariants_EE = [np.trace(np.dot(EE,M[i]*A[i])) for i in range(3)]
+    EEA = np.tensordot(EE,A)
+    EA = np.tensordot(E,A)
 
-    invariants_E = [np.trace(np.dot(E,A[i]))*np.trace(np.dot(E,A[j])) 
-                    for (i,j) in lin.utri_indices(3)]
-
-    return (2*sum(invariants_EE) + sum(invariants_E))/c
+    # Express model with parameters
+    # (2mi(Ai:E^2) + lij(Ai:E)(Aj:E))/c
+    M_part = 2*np.dot(M,EEA)
+    L_part = np.dot(L,lin.utri_flat(np.outer(EA,EA)))
+    return (M_part + L_part)/c
 
 
 def Sbar(E,c,m1,m2,m3,l11,l12,l13,l22,l23,l33,P=None):
-    P = P or standard_orthotropic_P()
+    P = P or el.standard_orthotropic_P()
+
+
     
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 def Cbar(E,c,m1,m2,m3,l11,l12,l13,l22,l23,l33,P=None):
-    P = P or standard_orthotropic_P()
+    P = P or el.standard_orthotropic_P()
     pass
 
 def C(E,c,m1,m2,m3,l11,l12,l13,l22,l23,l33,P=None):
-    P = P or standard_orthotropic_P()
+    P = P or el.standard_orthotropic_P()
     pass
 
