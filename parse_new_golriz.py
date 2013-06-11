6c1454fc5555dd19df0148f8e39759a91ed2a8a8
Med Mahmoud <med@temple.edu>
2013-06-11 19:05:37 -0400

    Clean golriz parser

--- parse_new_golriz.py
+++ parse_new_golriz.py
@@ -12,7 +12,7 @@ import lintools as lin
 
 
 # Global variables
-PREFIX = "/home/med/academic/Med-Golriz/Med/h4t7/"
+PREFIX = "/home/med/re/data/golriz-biaxial/"
 
 
 # Helper functions
@@ -54,79 +54,83 @@ def F_between(u1,u2,x1,x2):
 
 
 
-if __name__ == "__main__":
 
-    # Load data from data files
-    DCOORD = ExcelFile(PREFIX+"D_Coord.xlsx")
-    BASE = ExcelFile(PREFIX+"BaseInfo.xlsx")
-    FORCE = scipy.io.loadmat(PREFIX+"Force.mat")
+# Load data from data files
+DCOORD = ExcelFile(PREFIX+"D_Coord.xlsx")
+BASE = scipy.io.loadmat(PREFIX+"ZState.mat")
+FORCE = scipy.io.loadmat(PREFIX+"Force_h4t3.mat")
 
-    # Dimensions of the sample (stress-free?  prestretch?)
-    (Llc,Lll,thk) = [24.50, 21.70, 1.36]
+# Dimensions of the sample (stress-free?  prestretch?)
+(Llc,Lll,thk) = BASE['ZDim'].tolist()[0]
 
-    # Positions of markers in (stress-free?  prestretch?) configuration
-    # material_coords[marker, C/L]
-    material_coords = BASE.parse("ZCoord photo",header=None,skip_footer=1)\
-                          .as_matrix().reshape((11,2))
+# Positions of markers in (stress-free?  prestretch?) configuration
+# material_coords[marker, C/L]
+material_coords = BASE['ZCoord'].reshape((11,2))
 
-    # Circumferential and Longitudinal force at each timestep
-    # force_data[timestep, C/L]
-    force_data = np.array([FORCE['ForceC'][:,0],FORCE['ForceL'][:,0]]).T
+# Circumferential and Longitudinal force at each timestep
+# force_data[timestep, C/L]
+force_data = np.array([FORCE['ForceC'][:,0],FORCE['ForceL'][:,0]]).T
 
-    # Position of each marker at each timestep
-    # deformation_coords[timestep, marker, C/L]
-    deformation_coords = DCOORD.parse("h4t7",header=1,parse_cols="Y:AT")\
-                               .as_matrix().reshape((201,11,2))
+# Position of each marker at each timestep
+# deformation_coords[timestep, marker, C/L]
+deformation_coords = DCOORD.parse("h4t3",header=1,parse_cols="Y:AT")\
+                           .as_matrix()
+# Split up the flat point coordinates into a list of 2d points
+new_dc_shape = (len(deformation_coords), 11, 2)
+deformation_coords = deformation_coords.reshape(new_dc_shape)
 
-    # Get the scale taking deformation coords from pixels to mm
-    deformation_scale = BASE.parse("Scale",header=None).as_matrix()
+# Get the scale taking deformation coords from pixels to mm
+deformation_scale = BASE['scale']
 
-    # Rescale the deformation coordinates
-    deformation_coords = deformation_scale*deformation_coords
+# Rescale the deformation coordinates
+deformation_coords = deformation_scale*deformation_coords
 
-    ##################################################
-    # Compute the Lagrangian strain at each timestep # 
-    ##################################################
+##################################################
+# Compute the Lagrangian strain at each timestep # 
+##################################################
 
-    # Displacements of markers at each timestep
-    # displacements[timestep, marker, C/L]
-    displacements = deformation_coords - material_coords
+# Displacements of markers at each timestep
+# displacements[timestep, marker, C/L]
+displacements = deformation_coords - material_coords
 
-    # Compute the deformation gradient for each timestep by averaging the
-    # deformation gradients on edges which overlap roughly in the center
-    antipodal_nodes = [(1,11),(2,10),(3,8),(4,7),(5,9)]
-    # Convert to 0-indexed instead of 1-indexed
-    antipodal_nodes = [(i-1,j-1) for (i,j) in antipodal_nodes]
+# Compute the deformation gradient for each timestep by averaging the
+# deformation gradients on edges which overlap roughly in the center
+antipodal_nodes = [(1,11),(2,10),(3,8),(4,7),(5,9)]
+# Convert to 0-indexed instead of 1-indexed
+antipodal_nodes = [(i-1,j-1) for (i,j) in antipodal_nodes]
 
-    # Find the deformation gradients at each edge for each timestep
-    deformations = np.array([[F_between(u[i],u[j],
-                                        material_coords[i],material_coords[j]) 
-                     for (i,j) in antipodal_nodes] 
-                     for u in displacements])
+# Find the deformation gradients at each edge for each timestep
+deformations = np.array([[F_between(u[i],u[j],
+                                    material_coords[i],material_coords[j]) 
+                 for (i,j) in antipodal_nodes] 
+                 for u in displacements])
 
-    # Average the deformation gradients for all the edges at each timestep to
-    # get the average deformation gradient at that timestep
-    avg_deformations = np.average(deformations,axis=1)
+# Average the deformation gradients for all the edges at each timestep to
+# get the average deformation gradient at that timestep
+avg_deformations = np.average(deformations,axis=1)
 
-    # Compute the Lagrangian strain for each deformation
-    right_cauchy_green = np.einsum('...ab,...ac',avg_deformations
-                                                ,avg_deformations)
-    lagrangian_strain = 0.5*(right_cauchy_green - np.eye(3))
-    
+# Compute the Lagrangian strain for each deformation
+right_cauchy_green = np.einsum('...ab,...ac',avg_deformations
+                                            ,avg_deformations)
+lagrangian_strain = 0.5*(right_cauchy_green - np.eye(3))
 
-    ###########################################
-    # Compute the PK1 stress at each timestep # 
-    ###########################################
-    
-    # First, compute the areas that the forces are being applied to to get the
-    # principal stresses
-    # The units are (mm)^2, but we want to convert to SI, so we divide by 10^6
-    # EXCEPT we don't want Pa, we want kPa, so we only divide by 10^3!
-    areas = (thk*np.array([Llc,Lll,1]))/1e3
 
-    # We assume that the stress is perfectly biaxial
-    PK1 = np.array([np.diagflat(f.tolist()+[0]) for f in force_data])/areas
-    
+###########################################
+# Compute the PK1 stress at each timestep # 
+###########################################
 
-    
+# First, compute the areas that the forces are being applied to to get the
+# principal stresses
+# The units are (mm)^2, but we want to convert to SI, so we divide by 10^6
+# EXCEPT we don't want Pa, we want kPa, so we only divide by 10^3!
+areas = (thk*np.array([Llc,Lll,1]))/1e3
+
+# We assume that the stress is perfectly biaxial
+PK1 = np.array([np.diagflat(f.tolist()+[0]) for f in force_data])/areas
+
+# Let's also make a PK2 version so we can compare it with Fung more easily
+PK2 = np.array([np.dot(F,P) for (F,P) in zip(avg_deformations,PK1)])
+
+
+
 
