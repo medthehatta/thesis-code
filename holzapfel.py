6318d1576f5bb36a28e0e727b997589498f72edf
Med Mahmoud <med@temple.edu>
2013-06-25 16:00:39 -0400

    Update tangent stiff in holz

--- holzapfel.py
+++ holzapfel.py
@@ -49,3 +49,59 @@ def constitutive_model(C,A1,A2,*params):
     return isotropic_part + fiber_part1 + fiber_part2
 
 
+def tangent_stiffness(C,A1,A2,*params):
+    # Extract parameters from the parameter vector
+    (c,k1,k2) = params
+
+    # Compute the necessary basis tensors
+    I = np.eye(3)
+    Ci = np.linalg.inv(C)
+
+    # Define the index notation for the various products
+    tensor = '...ab,...cd->abcd'
+    kronecker = '...ac,...bd->abcd'
+    cokronecker = '...ad,...bc->abcd'
+
+    # Tensor products of the C's
+    CixCi = np.einsum(tensor,Ci,Ci)
+    CixI = np.einsum(tensor,Ci,I)
+    IxCi = np.einsum(tensor,I,Ci)
+    CivI = CixI + IxCi
+    CipCi = 0.5*np.einsum(kronecker,Ci,Ci) + \
+            0.5*(1/3.)*np.einsum(cokronecker,Ci,Ci)
+
+    # Tensor products of the A's
+    A1xA1 = np.einsum(tensor,A1,A1)
+    A2xA2 = np.einsum(tensor,A2,A2)
+
+    # Mixed products
+    CixA1 = np.einsum(tensor,Ci,A1)
+    A1xCi = np.einsum(tensor,A1,Ci)
+    CivA1 = CixA1 + A1xCi
+    CixA2 = np.einsum(tensor,Ci,A2)
+    A2xCi = np.einsum(tensor,A2,Ci)
+    CivA2 = CixA2 + A2xCi
+
+    # Alias the anisotropic invariants
+    I0 = np.trace(C)
+    I1 = np.tensordot(C,A1)
+    I2 = np.tensordot(C,A2)
+
+    # Alias the quantity (Ibar - 1)
+    # There is no J, because we assume J=1
+    Q1 = I1 - 1
+    Q2 = I2 - 1
+
+    # Collect the constituent terms
+    isotropic1 = (c/2)*(0.5*I0*CipCi - (1/3.)*CivI)
+    distortional01 = k1*np.exp(k2*Q1*Q1)
+    distortional02 = k1*np.exp(k2*Q2*Q2)
+    distortional11 = Q1*(0.5*I1*CipCi - (1/3.)*CivA1)
+    distortional12 = Q2*(0.5*I2*CipCi - (1/3.)*CivA2)
+    distortional21 = (1+2*k2*Q1*Q1)*(A1xA1 + (1/9.)*I1*I1*CixCi - (1/3.)*I1*CivA1)
+    distortional22 = (1+2*k2*Q2*Q2)*(A2xA2 + (1/9.)*I2*I2*CixCi - (2/3.)*I2*CivA2)
+
+    return isotropic1 + \
+           distortional01*(distortional11 + distortional21) + \
+           distortional02*(distortional12 + distortional22)
+
