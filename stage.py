774293e0f4de9d3a4a52ea5632978df0964af5ef
Med Mahmoud <med@temple.edu>
2013-06-26 12:08:51 -0400

    Clean stage, add helper vec->mat

--- stage.py
+++ stage.py
@@ -3,8 +3,12 @@ import lintools as lin
 
 def random_F(scale=1.0):
     F0 = np.eye(2) + scale*np.random.random((2,2))
-    J0 = np.linalg.det(F0)
-    F = lin.direct_sum(F0,np.diagflat([1/J0]))
-    return F
+    return det1_3d(F0)
 
+def symmetric_2_1(a11,a22,a12):
+    return np.array([[a11,a12],[a12,a22]])
+
+def det1_3d(mat2d):
+    J0 = np.linalg.det(mat2d)
+    return lin.direct_sum(mat2d,np.diagflat([1/J0]))
 
