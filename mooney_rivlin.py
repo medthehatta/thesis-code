1657c94cc4b86f2ee14799673dc8f6a29199adfb
Med Mahmoud <med@temple.edu>
2013-07-08 18:04:43 -0400

    Change to 3-term constitutive model and remove spatial tstiff

--- mooney_rivlin.py
+++ mooney_rivlin.py
@@ -7,52 +7,6 @@
 
 import numpy as np
 
-def spatial_tangent_stiffness(b,pressure=0,*p):
-    """
-    Spatial Tangent stiffness as a function of the left Cauchy-green tensor and
-    the two parameters: p=c1,c2
-    """
-    
-    # Extract the model parameters
-    (c1,c2) = p
-
-    # Compute the other required generating tensors for the expression
-    bb = np.dot(b,b)
-    I = np.eye(3)
-
-    # Alias the scalar invariants of b
-    I1 = np.trace(b)
-    I2 = 0.5*(I1*I1 - np.trace(np.dot(b,b)))
-
-    # Alias the index manipulations for the various tensor products
-    tensor = '...ab,...cd->abcd'
-    kronecker = '...ac,...bd->abcd'
-    cokronecker = '...ad,...cb->abcd'
-
-    # Compute the relevant products of I
-    IxI = np.einsum(tensor,I,I)
-    IsI = 0.5*(np.einsum(kronecker,I,I) + np.einsum(cokronecker,I,I))
-
-    # Compute the relevant products of b
-    bxb = np.einsum(tensor,b,b)
-    bsb = 0.5*(np.einsum(kronecker,b,b) + np.einsum(cokronecker,b,b))
-
-    # Compute the mixed products of b, I, and bb
-    bxI = np.einsum(tensor,b,I)
-    Ixb = np.einsum(tensor,I,b)
-    bvI = bxI + Ixb
-    bbxI = np.einsum(tensor,bb,I)
-    Ixbb = np.einsum(tensor,I,bb)
-    bbvI = bbxI + Ixbb
-
-    # Assemble the expression
-    part1 = (1/3.)*(I1*(IsI + (1/3.)*IxI) - bvI)
-    part2 = (2/3.)*(bbvI - I1*bvI + I2*(IsI + (1/3.)*IxI))
-    part3 = (2/3.)*(bxb - (0.5)*bsb)
-
-    return -pressure*np.eye(3) + c1*part1 + c2*(part2 + part3)
-
-
 def material_tangent_stiffness(C,pressure,*p):
     """
     Material Tangent stiffness as a function of the right Cauchy-green tensor
@@ -100,14 +54,14 @@ def material_tangent_stiffness(C,pressure,*p):
     return pressure*part0 + 4*(c1*(1/3.)*part1 + c2*(2/3.)*part21 + c2*part22)
 
 
-def constitutive_model(b,*p):
+def constitutive_model(b,pressure,*p):
     """
-    Distortional Kirchhoff stress as a function of the left Cauchy-green tensor
-    and the two parameters: p=c1,c2
+    Kirchhoff stress as a function of the left Cauchy-green tensor and the
+    parameters: params=c1,c2,c3
     """
    
     # Extract the model parameters
-    (c1,c2) = p
+    (c1,c2,c3) = p
 
     # Compute the other required generating tensors for the expression
     I = np.eye(3)
@@ -118,11 +72,14 @@ def constitutive_model(b,*p):
     I2 = 0.5*(I1*I1 - np.trace(np.dot(b,b)))
 
     # Assemble the expression
-    dIb1db = b - (1/3.)*I1*I
-    dIb2db = I1*b - (2/3.)*I2*I - bb
-    dWdb = c1*dIb1db + c2*dIb2db
+    volumetric = -pressure*I
+    part1 = 2*c1*b
+    part2 = 2*c2*(I1*b - bb)
+    part31 = I2*b + I1*I1*b - I1*bb
+    part32 = bb - I1*b - b
+    part3 = 6*c3*(part31/3. + part32)
 
-    return 2*dWdb
+    return part1 + part2 + part3
 
 
 
