f9d4fad0904e598dee141e73f2f06d9d8b37db26
Med Mahmoud <medthehatta@gmail.com>
2013-07-14 00:11:45 -0400

    Try to smooth eigvec selection

--- follow_positive_eigvals.py
+++ follow_positive_eigvals.py
@@ -9,16 +9,26 @@ def tstiff(F, *p):
     pressure = el.pressure_PK1(F, mr.constitutive_model, *p)
     return mr.material_tangent_stiffness(F, pressure, *p)
 
+def const(F, *p):
+    pressure = el.pressure_PK1(F, mr.constitutive_model, *p)
+    return mr.constitutive_model(F, pressure, *p)
+
+def sedf(F, *p):
+    return mr.strain_energy_density(F, *p)
+
 def voigt_vec_to_mat(d):
     return np.array([[d[0],d[5],d[4]],
                      [d[5],d[1],d[3]],
                      [d[4],d[3],d[2]]]) 
 
 
-dt = 0.01
+dt = 0.003
+params = [3.,-1.]
+
 F = np.eye(3)
 Fs = [F]
-for j in range(1000):
+selecteds = []
+for j in range(200):
     # Compute D
     D = el.voigt(tstiff(F,*params))
 
@@ -27,10 +37,18 @@ for j in range(1000):
     eigsystem = zip(eigvals,eigvecs)
     candidates = [vec for (val,vec) in eigsystem if val>0]
     if len(candidates)==0: break
-    d = candidates[0]
+
+    # Try to pick the eigenvector with the closest eigenvalue to the last
+    # picked
+    if selecteds != []:
+        diffs = [np.abs(c-selected[-1]) for c in candidates]
+        selected = candidates[np.argmin(diffs)]
+        selecteds.append(selected)
+    else:
+        selected = candidates[0]
 
     # Convert to matrix
-    mat = voigt_vec_to_mat(d)
+    mat = voigt_vec_to_mat(selected)
 
     # Exponentiate (is this necessary?)
     Q = expm(dt*mat)
@@ -39,5 +57,7 @@ for j in range(1000):
     F = np.dot(Q,np.dot(F,Q.T))
     Fs.append(F)
 
+Ws = [sedf(F,*params) for F in Fs]
+Ps = [const(F,*params) for F in Fs]
 
 
