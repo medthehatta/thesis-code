9d458dcd065fb92dc41f9956da12259520f05bf9
Med Mahmoud <med@temple.edu>
2013-07-22 21:27:00 -0400

    Add Holzapfel-style derivation of tstiff (probably correct)

--- mooney_rivlin.py
+++ mooney_rivlin.py
@@ -60,6 +60,65 @@ def iso_material_elasticity(F,H,*params):
     return 4*(c10*(1/3.)*part1 + c01*part2)
 
 
+def iso_material_elasticity_2(F,H,*params): 
+
+    # Extract the model parameters
+    (c10,c01) = params
+
+    # Compute the other required generating tensors for the expression
+    C = np.dot(F.T,F)
+    Ci = np.linalg.inv(C)
+    I = np.eye(3)
+    So = iso_material_model(F,H,*params)
+
+    # Alias the scalar invariants of C
+    I1 = np.trace(C)
+    I2 = 0.5*(I1*I1 - np.trace(np.dot(C,C)))
+
+    # Alias the index manipulations for the various tensor products
+    tensor = '...ab,...cd->abcd'
+    kronecker = '...ac,...bd->abcd'
+    cokronecker = '...ad,...bc->abcd'
+
+    # Compute the relevant products of I
+    IxI = np.einsum(tensor,I,I)
+    IsI = 0.5*(np.einsum(kronecker,I,I) + np.einsum(cokronecker,I,I))
+
+    # Compute the relevant products of C and Ci
+    CixCi = np.einsum(tensor,Ci,Ci)
+    CixC = np.einsum(tensor,Ci,C)
+    CxCi = np.einsum(tensor,C,Ci)
+    CvCi = CxCi + CixC
+    CisCi = 0.5*(np.einsum(kronecker,Ci,Ci) + np.einsum(cokronecker,Ci,Ci))
+
+    # Compute the mixed products of C, I, and Ci
+    CixI = np.einsum(tensor,Ci,I)
+    IxCi = np.einsum(tensor,I,Ci)
+    CivI = CixI + IxCi
+    CxI = np.einsum(tensor,C,I)
+    IxC = np.einsum(tensor,I,C)
+    CvI = CxI + IxC
+
+    # Compute the projection tensors
+    PP = IsI - (1/3.)*CixC
+    PPt = IsI - (1/3.)*CxCi
+    PPx = CisCi - (1/3.)*CixCi
+
+    # Compute PPSo
+    PPSo = np.einsum('abcd,cd',PP,So)
+
+    # Compute CivPPSo
+    CixPPSo = np.einsum(tensor,Ci,PPSo)
+    PPSoxCi = np.einsum(tensor,PPSo,Ci)
+    CivPPSo = CixPPSo + PPSoxCi
+
+    part1 = 4*c01*np.einsum('abcd,cdef,efgh',PP,(IxI - IsI),PPt)
+    part2 = (2/3.)*np.tensordot(So,C)*PPx
+    part3 = -(2/3.)*CivPPSo
+
+    return part1 + part2 + part3
+
+
 def iso_material_model(F,H,*params):
     """
     Isochoric PK2 stress as a function of the deformation gradient and model
@@ -105,3 +164,5 @@ def vol_material_elasticity(F,p,pt,H,*params):
     C = np.dot(F.T,F)
     Ci = np.linalg.inv(C)
     return pt*lin.tensor(Ci,Ci) - 2*p*lin.symmetric_kronecker(Ci,Ci)
+
+
